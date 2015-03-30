3aa18a917154e5ac739d124333ac0cd6978fdeca
stevenmccall <steventmccall@gmail.com>
2015-03-30 11:14:14 -0500

    Implementing Autologin

--- src/com/txstatecs4398/coursemate/WebViewActivity.java
+++ src/com/txstatecs4398/coursemate/WebViewActivity.java
@@ -14,7 +14,10 @@ public class WebViewActivity extends Activity {
 
     private WebView webView;
     public boolean done = false;
+    String username = "username";
+    String password = "password";
     //public boolean doneTrail = false;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,32 +25,38 @@ public class WebViewActivity extends Activity {
         webView = (WebView) findViewById(R.id.webView1);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setUserAgentString("Mozilla/5.0 (Linux; U; Android 4.0.4; en-gb; GT-I9100 Build/IMM76D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30");
-        
+
         // intercept calls to console.log
         webView.setWebChromeClient(new WebChromeClient() {
 
             @Override
             public boolean onConsoleMessage(ConsoleMessage cmsg) {
-                if (cmsg.message().startsWith("MAGIC") && done) 
-                {
-                    passStream("DONE DONE DONE"+cmsg.message().substring(5));
+                if (cmsg.message().startsWith("MAGIC") && done) {
+                    passStream(cmsg.message().substring(5));
                     //return true;
                 }
                 return true;
             }
+
             @Override
-            public void onProgressChanged(WebView view, int newProgress)
-            {
-                if(newProgress == 100) //passStream("reaches 100");
+            public void onProgressChanged(WebView view, int newProgress) {
+                if (newProgress == 100 && done) {
                     view.loadUrl("javascript:console.log('MAGIC'+document.getElementsByTagName('html')[0].innerHTML);");
+                } else if (newProgress == 100 && !done) {
+                    view.loadUrl("javascript: {"
+                            + "document.getElementById('UserID').value = '" + username + "';"
+                            + "document.getElementById('PIN').value = '" + password + "';"
+                            + "var frms = document.getElementsByName('Login');"
+                            + "frms[0].submit(); };");
+                }
+
             }
         });
-        webView.setWebViewClient(new WebViewClient(){
+        webView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 //if(done)doneTrail = true;
-                if (url.contains("https://ssb.txstate.edu/prod/twbkwbis.P_GenMenu?name=bmenu.P_MainMnu&msg=WELCOME"))
-                {
+                if (url.contains("https://ssb.txstate.edu/prod/twbkwbis.P_GenMenu?name=bmenu.P_MainMnu&msg=WELCOME")) {
                     done = true;
                     url = "https://ssb.txstate.edu/prod/bwskfshd.P_CrseSchd";
                     view.loadUrl(url);
@@ -55,7 +64,8 @@ public class WebViewActivity extends Activity {
                     view.loadUrl(url);
                 }
                 return true;
-            }});
+            }
+        });
         webView.loadUrl("https://ssb.txstate.edu/prod/twbkwbis.P_ValLogin");
     }
 
@@ -64,7 +74,7 @@ public class WebViewActivity extends Activity {
                 + "('<html>'+document.getElementsByTagName('html')[0].innerHTML+'</html>');");
 
         Logger.getLogger(WebViewActivity2.class.getName()).log(Level.SEVERE, null);
-       
+
         Intent intent = new Intent(WebViewActivity.this, MainActivity.class);
         intent.putExtra("HTMLStream", HTMLStream);  //used to pass data
         startActivity(intent);
