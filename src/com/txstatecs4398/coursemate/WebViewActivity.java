6948021a24724c6bcd958df20550766f5d430822
Steven McCall <steventmccall@gmail.com>
2015-03-30 00:14:21 -0600

    Further cleaning of WebViewActivity.java

--- src/com/txstatecs4398/coursemate/WebViewActivity.java
+++ src/com/txstatecs4398/coursemate/WebViewActivity.java
@@ -14,17 +14,6 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.apache.http.HttpVersion;
-import org.apache.http.client.HttpClient;
-import org.apache.http.conn.scheme.PlainSocketFactory;
-import org.apache.http.conn.scheme.Scheme;
-import org.apache.http.conn.scheme.SchemeRegistry;
-import org.apache.http.conn.ssl.SSLSocketFactory;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpParams;
-import org.apache.http.params.HttpProtocolParams;
 
 public class WebViewActivity extends Activity {
 
@@ -37,10 +26,10 @@ public class WebViewActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.webview);
-        DefaultHttpClient testClient = (DefaultHttpClient) getHttpClient();
 
         // intercept calls to console.log
         webView.setWebChromeClient(new WebChromeClient() {
+            @Override
             public boolean onConsoleMessage(ConsoleMessage cmsg) {
                 // check secret prefix
                 if (cmsg.message().startsWith("MAGIC")) 
@@ -53,6 +42,7 @@ public class WebViewActivity extends Activity {
         });
 
         webView.setWebViewClient(new WebViewClient() {
+            @Override
             public void onPageFinished(WebView view, String address) {
                 // have the page spill its guts, with a secret prefix
                 view.loadUrl("javascript:console.log('MAGIC'+document.getElementsByTagName('html')[0].innerHTML);");
@@ -71,26 +61,5 @@ public class WebViewActivity extends Activity {
         Intent intent = new Intent(WebViewActivity.this, MainActivity.class);
         intent.putExtra("HTMLStream", HTMLStream);  //used to pass data
         startActivity(intent);
-    }
-
-    private static HttpClient getHttpClient() {
-        DefaultHttpClient ret = null;
-
-        //sets up parameters
-        HttpParams params = new BasicHttpParams();
-        HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
-        HttpProtocolParams.setContentCharset(params, "utf-8");
-        params.setBooleanParameter("http.protocol.expect-continue", false);
-
-        //registers schemes for both http and https
-        SchemeRegistry registry = new SchemeRegistry();
-        registry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
-        final SSLSocketFactory sslSocketFactory = SSLSocketFactory.getSocketFactory();
-        sslSocketFactory.setHostnameVerifier(SSLSocketFactory.BROWSER_COMPATIBLE_HOSTNAME_VERIFIER);
-        registry.register(new Scheme("https", sslSocketFactory, 443));
-
-        ThreadSafeClientConnManager manager = new ThreadSafeClientConnManager(params, registry);
-        ret = new DefaultHttpClient(manager, params);
-        return ret;
     }
 }
