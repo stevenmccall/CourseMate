6d0f7bd9347e1274de8f33cd6151c841f28dc1e1
stevenmccall <steventmccall@gmail.com>
2015-04-01 02:47:06 -0500

    parser
    parser added, not properly integrated yet.

--- src/com/txstatecs4398/coursemate/MainActivity.java
+++ src/com/txstatecs4398/coursemate/MainActivity.java
@@ -7,11 +7,13 @@ import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity
 {
     private TextView text1;
+    private EditText user1;
     private Button button;
     
     @Override
@@ -26,6 +28,7 @@ public class MainActivity extends Activity
             HTMLStream = extras.getString("HTMLStream");
         }
         
+        user1 = (EditText) findViewById(R.id.username1);
         text1 = (TextView) findViewById(R.id.text1); 
         text1.setMovementMethod(new ScrollingMovementMethod());
         text1.setText("Press the button below:");
@@ -37,6 +40,7 @@ public class MainActivity extends Activity
             public void onClick(View arg0) 
                 { 
                     Intent intent = new Intent(MainActivity.this, WebViewActivity.class);
+                    intent.putExtra("username", user1.getText().toString());  //used to pass data
                     startActivity(intent);
                 }
         });	
