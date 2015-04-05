3b5923a50758d239aaba9fdd93d2da2301b13311
stevenmccall <steventmccall@gmail.com>
2015-04-04 20:20:38 -0500

    NFC implemented
    NFC fully integrated
    Need to setup interaction for received data.

--- gen/com/txstatecs4398/coursemate/R.java
+++ gen/com/txstatecs4398/coursemate/R.java
@@ -18,13 +18,13 @@ public final class R {
         public static final int supercat=0x7f020004;
     }
     public static final class id {
-        public static final int buttonUrl=0x7f060004;
-        public static final int calendar1=0x7f060005;
-        public static final int image1=0x7f060000;
-        public static final int password1=0x7f060003;
-        public static final int text2=0x7f060006;
-        public static final int username1=0x7f060002;
-        public static final int webView1=0x7f060001;
+        public static final int buttonUrl=0x7f070004;
+        public static final int calendar1=0x7f070005;
+        public static final int image1=0x7f070000;
+        public static final int password1=0x7f070003;
+        public static final int text2=0x7f070006;
+        public static final int username1=0x7f070002;
+        public static final int webView1=0x7f070001;
     }
     public static final class layout {
         public static final int html_loader=0x7f030000;
@@ -32,9 +32,12 @@ public final class R {
         public static final int main=0x7f030002;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
+        public static final int app_name=0x7f050000;
     }
     public static final class style {
-        public static final int button_text=0x7f050000;
+        public static final int button_text=0x7f060000;
+    }
+    public static final class xml {
+        public static final int nfc_tech_filter=0x7f040000;
     }
 }
--- src/com/txstatecs4398/coursemate/PostLoginMainActivity.java
+++ src/com/txstatecs4398/coursemate/PostLoginMainActivity.java
@@ -9,45 +9,97 @@ import android.widget.TextView;
 import com.txstatecs4398.coursemate.meetingshared.IndividualSchedule;
 import com.txstatecs4398.coursemate.meetingshared.ParseBuff;
 
-public class PostLoginMainActivity extends Activity
-{
+import java.nio.charset.Charset;
+import java.util.Locale;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+
+public class PostLoginMainActivity extends Activity {
+
     private TextView text1;
     private IndividualSchedule person;
     private CalendarView calendar1;
 
+    private NfcAdapter mAdapter;
+    private TextView mText;
+    private NdefMessage mMessage;
+
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {        
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
         String HTMLStream, netID;
         Bundle extras = getIntent().getExtras();
-        
+
         calendar1 = (CalendarView) findViewById(R.id.calendar1);
         calendar1.setShowWeekNumber(false);
 
-        text1 = (TextView) findViewById(R.id.text2); 
+        text1 = (TextView) findViewById(R.id.text2);
         text1.setMovementMethod(new ScrollingMovementMethod());
         text1.setText("HTMLStream:");
-        
-        if (extras != null) 
-        {
+
+        if (extras != null) {
             HTMLStream = extras.getString("HTMLStream");
             netID = extras.getString("netID");
-            ParseBuff test = new ParseBuff(netID); 
+            ParseBuff test = new ParseBuff(netID);
             person = test.parse(HTMLStream);
-            text1.append(person.showSched());
+            text1.setText(person.showSched());
+
+            mAdapter = NfcAdapter.getDefaultAdapter(this);
+
+            if (mAdapter != null) {
+                text1.append("Tap another Android phone with NFC to push 'NDEF Push Sample'");
+            } else {
+                mText.append("This phone is not NFC enabled.");
+            }
+
+            // Create an NDEF message with some sample text
+            mMessage = new NdefMessage(
+                    new NdefRecord[]{newTextRecord(person.getNetID()+" "+person.showSched(), Locale.ENGLISH, true)});
+
         }
-        
-        calendar1.setOnDateChangeListener(new CalendarView.OnDateChangeListener() 
-        {
+
+        calendar1.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) 
-            {
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
                 //check the day of month against a custom class storing schedule availability.
                 //show a dialog with a list view to time frames.
             }
         });
+    }
+
+    public static NdefRecord newTextRecord(String text, Locale locale, boolean encodeInUtf8) {
+        byte[] langBytes = locale.getLanguage().getBytes(Charset.forName("US-ASCII"));
+
+        Charset utfEncoding = encodeInUtf8 ? Charset.forName("UTF-8") : Charset.forName("UTF-16");
+        byte[] textBytes = text.getBytes(utfEncoding);
+
+        int utfBit = encodeInUtf8 ? 0 : (1 << 7);
+        char status = (char) (utfBit + langBytes.length);
+
+        byte[] data = new byte[1 + langBytes.length + textBytes.length];
+        data[0] = (byte) status;
+        System.arraycopy(langBytes, 0, data, 1, langBytes.length);
+        System.arraycopy(textBytes, 0, data, 1 + langBytes.length, textBytes.length);
+
+        return new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_TEXT, new byte[0], data);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mAdapter != null) {
+            mAdapter.enableForegroundNdefPush(this, mMessage);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mAdapter != null) {
+            mAdapter.disableForegroundNdefPush(this);
+        }
     }
 }
