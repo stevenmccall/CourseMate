a8ac1cc851e6c9d4b21ac67320284487243943b7
stevenmccall <steventmccall@gmail.com>
2015-04-12 08:03:46 -0500

    Group Creator Added
    Added functionality of making groups.
    -Still in progress, but groups are created and saved to phone.

--- bin/AndroidManifest.xml.d
+++ bin/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-C:\Users\Jack\Documents\NetBeansProjects\CourseMate\bin\AndroidManifest.xml : \
-C:\Users\Jack\Documents\NetBeansProjects\CourseMate\AndroidManifest.xml \
+C:\Users\theoj_000\Documents\NetBeansProjects\CourseMate\bin\AndroidManifest.xml : \
+C:\Users\theoj_000\Documents\NetBeansProjects\CourseMate\AndroidManifest.xml \
--- src/com/txstatecs4398/coursemate/GroupSelectionActivity.java
+++ src/com/txstatecs4398/coursemate/GroupSelectionActivity.java
@@ -6,16 +6,26 @@
 package com.txstatecs4398.coursemate;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FilenameFilter;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -26,10 +36,14 @@ import java.util.logging.Logger;
  */
 public class GroupSelectionActivity extends Activity {
 
+    final Context context = this;
     private TextView view;
     private Button addButton;
+    private Button logoutButton;
+    private Button shareButton;
     private String nfcNetID;
     private String nfcSched;
+    private AlertDialog alertDialog;
 
     /**
      * Called when the activity is first created.
@@ -42,7 +56,6 @@ public class GroupSelectionActivity extends Activity {
         Bundle extras = getIntent().getExtras();
 
         view = (TextView) findViewById(R.id.text1);
-        addButton = (Button) findViewById(R.id.addGroup);
         // end of view setup code
 
         if (extras != null) {
@@ -51,34 +64,100 @@ public class GroupSelectionActivity extends Activity {
             view.append(nfcNetID + "\n");
             view.append(nfcSched);
         }
-
-        addButton = (Button) findViewById(R.id.passSched);
-        addButton.setOnClickListener(new View.OnClickListener() {
+        //----------Start of Group Processing--------------
+        
+        if(groupRetriever())
+        {
+        }
+        
+        //----------start of buttons-----
+        shareButton = (Button) findViewById(R.id.passSched);
+        shareButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
                 Intent intent = new Intent(GroupSelectionActivity.this, ShareActivity.class);
                 startActivity(intent);
             }
         });
-        
-        addButton = (Button) findViewById(R.id.logout);
-        addButton.setOnClickListener(new View.OnClickListener() {
+
+        logoutButton = (Button) findViewById(R.id.logout);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
                 deleteFile("user");
                 Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
                 startActivity(intent);
             }
         });
+
+        // add button listener
+        addButton = (Button) findViewById(R.id.addGroup);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                dialogCreate();
+                alertDialog.show();
+            }
+        });
+    }
+    
+    public void dialogCreate()
+    {
+        // get prompts.xml view
+                LayoutInflater li = LayoutInflater.from(context);
+                View promptsView = li.inflate(R.layout.dialog, null);
+
+                ContextThemeWrapper ctw = new ContextThemeWrapper( this, R.drawable.dialog );
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(ctw);
+
+                // set prompts.xml to alertdialog builder
+                alertDialogBuilder.setView(promptsView);
+                
+                final EditText userInput = (EditText) promptsView.findViewById(R.id.userGroup);
+
+                // set dialog message
+                alertDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        if(!userInput.getText().toString().isEmpty())
+                                            if(groupCreate(userInput.getText().toString()))
+                                                groupRetriever();
+                                    }
+                                })
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+                alertDialog = alertDialogBuilder.create();
+    }
+    
+    public boolean groupCreate(String filename) 
+    {
+        try {            
+            FileOutputStream fs = openFileOutput("CMG"+filename, Context.MODE_PRIVATE);
+            OutputStreamWriter ow = new OutputStreamWriter(fs);
+            BufferedWriter writer = new BufferedWriter(ow);
+            writer.flush();
+            writer.close();
+        } 
+        catch (Exception e){return false;}
+        
+        return true;
     }
 
     public boolean groupRetriever() {
+        view.setText("");
         File root = getFilesDir();
 
         FilenameFilter beginswithm = new FilenameFilter() {
             public boolean accept(File directory, String filename) {
-                return filename.startsWith("CM");
+                return filename.startsWith("CMG");
             }
         };
 
@@ -90,11 +169,13 @@ public class GroupSelectionActivity extends Activity {
 
         for (File file : files) {
             try {
+                view.append(file.getName().substring(3)+"\n");
                 Scanner in = new Scanner(file);
-                this.view.append(in.next());//groupName
+                
                 while (in.hasNext()) {
                     this.view.append(in.next());//personID
-                    in.nextLine();//schedule
+                    in.next();
+                    in.next();//schedule
                 }
             } catch (FileNotFoundException ex) {
                 Logger.getLogger(LoginActivity.class.getName()).log(Level.SEVERE, null, ex);
